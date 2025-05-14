56ec400cc78e4adf0422ae1bcf112e53d88360a0
adattoweb <adattostudio174@gmail.com>
2025-05-14 11:39:59 +0300

    update lang

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -10,7 +10,10 @@ export default function Header({ isEn, setIsEn }) {
         <li className={url[url.length - 1] === "/" || url[url.length - 1] === "#" ? "headitem active" : "headitem"}><Link to="/poemus/"><img className="icon" src="/poemus/home.png" /></Link></li>
         <li className={url.includes("history") ? "headitem active" : "headitem"}><Link to="/poemus/history"><img className="icon" src="/poemus/history.png" /></Link></li>
         <li className={url.includes("info") ? "headitem active" : "headitem"}><Link to="/poemus/info"><img className="icon" src="/poemus/info.png" /></Link></li>
-        <li className="headitem"><p className="mon text-[20px] font-bold w-[25px] cursor-pointer" onClick={() => setIsEn(!isEn)}>{isEn ? "UA" : "EN"}</p></li>
+        <li className="headitem"><p className="mon text-[20px] font-bold w-[25px] cursor-pointer" onClick={() => {
+          setIsEn(!isEn)
+          localStorage.setItem("poemus-lang", `${isEn ? "ua" : "en"}`)
+        }}>{isEn ? "UA" : "EN"}</p></li>
       </ul>
     </div>)
 }
